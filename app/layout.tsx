286303b4db073c4b515617d2d70607c17e0486ae
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-09-20 21:59:08 +0300

    fix project name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,40 @@
 import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
 import './globals.css';
 
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
-import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'NoteHub',
-  description: 'A simple application for managing personal notes',
+  description: 'A simple and convenient application for managing notes.',
+  openGraph: {
+    title: 'NoteHub',
+    description: 'A simple and convenient application for managing notes.',
+    url: 'https://08-zustand.vercel.app',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>
-        <TanStackProvider>
-          <Header />
-          {children}
-          {modal}
-          <Footer />
-        </TanStackProvider>
-      </body>
+      <body className={roboto.variable}>{children}</body>
     </html>
   );
 }
